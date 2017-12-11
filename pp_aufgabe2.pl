8492aeafa7c08d7a282e77d4c1706e58fa12aca0
v.bernardo-ciddio <v.b.ciddio@casando.de>
2017-12-11 16:16:34 +0100

    Init Project

---
+++ pp_aufgabe2.pl
@@ -0,0 +1,21 @@
+unartig(susi).
+unartig(fridolin).
+unartig(ralf).
+
+artig(hanna).
+artig(britta).
+
+sehrartig(klaus).
+sehrartig(sina).
+
+wert(schaukelpferd, 100).
+wert(kreisel, 20).
+wert(buntstifte, 40).
+wert(teddybaer, 60).
+wert(ritterburg, 110).
+wert(bausteine, 90).
+wert(buch, 40).
+wert(fahrrad, 200).
+wert(gameboy, 250).
+wert(puppe, 140).
+wert(murmeln, 30).
